433a4b13d3796cb3393925e0787fa3f4718d2843
br0steen <osteensco@gmail.com>
2023-10-24 21:59:49 -0500

    vimbegood installed

--- init.lua
+++ init.lua
@@ -32,7 +32,7 @@ require('lazy').setup({
   -- Git related plugins
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
-
+  'ThePrimeagen/vim-be-good',  
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 

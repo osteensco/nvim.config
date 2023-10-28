667b8e1b79a0eebc8e0bed2b85d3833c36d57c1f
br0steen <osteensco@gmail.com>
2023-10-27 21:39:08 -0500

    relative nums

--- lua/kickstart/init.lua
+++ lua/kickstart/init.lua
@@ -197,8 +197,9 @@ require('lazy').setup({
 -- Set highlight on search
 vim.o.hlsearch = false
 
--- Make line numbers default
+-- Make line numbers default or relative
 vim.wo.number = true
+vim.wo.relativenumber = true
 
 -- Enable mouse mode
 vim.o.mouse = 'a'

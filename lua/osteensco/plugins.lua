ffc736f9fc9af60dd65dcd54aae1dee5a074b713
br0steen <osteensco@gmail.com>
2023-11-02 20:50:16 -0500

    lsp and cmp

--- lua/osteensco/plugins.lua
+++ lua/osteensco/plugins.lua
@@ -29,11 +29,23 @@ return {
     	build = ":TSUpdate",
     },
     'ThePrimeagen/vim-be-good',
+
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
     "neovim/nvim-lspconfig",
 
+    'folke/neodev.nvim', 
+
+    {
+        'hrsh7th/nvim-cmp',
+        dependencies = {
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip',
+            'rafamadriz/friendly-snippets',
+            
+            'hrsh7th/cmp-nvim-lsp',
+            
+        },
+    },
+
 }
-
-
-

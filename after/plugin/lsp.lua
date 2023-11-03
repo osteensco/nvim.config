534015abefa4c58b3701d041a58e70589c6cb390
br0steen <osteensco@gmail.com>
2023-11-02 21:59:49 -0500

    keymaps desc for fuzzy finding

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,23 +1,23 @@
                                                                                                                     
 local on_attach = function(_, bufnr)
 
-  local bufmap = function(keys, func)
-    vim.keymap.set('n', keys, func, { buffer = bufnr })
+  local bufmap = function(keys, func, desc)
+    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
   end
 
-  bufmap('<leader>r', vim.lsp.buf.rename)
-  bufmap('<leader>a', vim.lsp.buf.code_action)
+  bufmap('<leader>r', vim.lsp.buf.rename, 'buf.rename')
+  bufmap('<leader>a', vim.lsp.buf.code_action, 'buf.code_action')
 
-  bufmap('gd', vim.lsp.buf.definition)
-  bufmap('gD', vim.lsp.buf.declaration)
-  bufmap('gI', vim.lsp.buf.implementation)
-  bufmap('<leader>D', vim.lsp.buf.type_definition)
+  bufmap('gd', vim.lsp.buf.definition, '[g]o to [d]efinition')
+  bufmap('gD', vim.lsp.buf.declaration, '[g]o to [D]eclaration')
+  bufmap('gI', vim.lsp.buf.implementation, '[g]o to [I]mplementation')
+  bufmap('<leader>D', vim.lsp.buf.type_definition, 'type_definition')
 
-  bufmap('gr', require('telescope.builtin').lsp_references)
-  bufmap('<leader>s', require('telescope.builtin').lsp_document_symbols)
-  bufmap('<leader>S', require('telescope.builtin').lsp_dynamic_workspace_symbols)
+  bufmap('gr', require('telescope.builtin').lsp_references, '[g]o to TS [r]eferences')
+  bufmap('<leader>s', require('telescope.builtin').lsp_document_symbols, 'TS lsp_document_symbols')
+  bufmap('<leader>S', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'TS lsp_dynamic_workspace_symbols')
 
-  bufmap('K', vim.lsp.buf.hover)
+  bufmap('K', vim.lsp.buf.hover, 'hover docs')
 
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     vim.lsp.buf.format()
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,11 +1,11 @@
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = '[f]ind [f]iles, looks at all files'})
+vim.keymap.set('n', '<leader>pf', builtin.git_files, {desc = '[p]roject [f]iles, looks at files within git repo'})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
-vim.keymap.set('n', '<leader>td', builtin.diagnostics, {})
+end, {desc = '[p]roject search, takes a string a searches for it in files within git repo'})
+vim.keymap.set('n', '<leader>td', builtin.diagnostics, {desc = '[t]elescope [d]iagnostics'})
 
 
 

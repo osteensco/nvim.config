0e9307ad370b4b55f0aaf57ead2ebf55d1cce8dd
br0steen <osteensco@gmail.com>
2023-11-05 13:54:42 -0600

    cd keymap

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,8 +1,8 @@
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = '[f]ind [f]iles, looks at all files'})
-vim.keymap.set('n', '<leader>pf', builtin.git_files, {desc = '[p]roject [f]iles, looks at files within git repo'})
-vim.keymap.set('n', '<leader>ps', function()
+vim.keymap.set('n', '<leader>tff', builtin.find_files, {desc = '[t]elescope [f]ind [f]iles, looks at all files'})
+vim.keymap.set('n', '<leader>tpf', builtin.git_files, {desc = '[t]elescope [p]roject [f]iles, looks at files within git repo'})
+vim.keymap.set('n', '<leader>tps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end, {desc = '[p]roject search, takes a string a searches for it in files within git repo'})
 vim.keymap.set('n', '<leader>td', builtin.diagnostics, {desc = '[t]elescope [d]iagnostics'})
--- lua/osteensco/remap.lua
+++ lua/osteensco/remap.lua
@@ -4,4 +4,5 @@ vim.keymap.set({'n', 'i', 'v'}, '<C-s>', '<Esc>:w<Enter>')
 vim.keymap.set({'n', 'i', 'v'}, '<C-_>', '<Cmd>set operatorfunc=CommentOperator<CR>g@')
 vim.keymap.set('n', '<leader>dvo', '<Cmd>DiffviewOpen<CR>')
 vim.keymap.set('n', '<leader>dvc', '<Cmd>DiffviewClose<CR>')
+vim.keymap.set('n', '<leader>cd', ':cd ')
 

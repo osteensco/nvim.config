af1d12dc82f55c629a1c41a4958730db06f53a9f
br0steen <osteensco@gmail.com>
2023-11-05 20:48:05 -0600

    remaps

--- lua/osteensco/remap.lua
+++ lua/osteensco/remap.lua
@@ -1,10 +1,18 @@
+
 vim.g.mapleader = " "
 vim.keymap.set('n', '<leader>w', '<C-w>')
 vim.keymap.set({'n', 'i', 'v'}, '<C-s>', '<Esc>:w<Enter>')
 vim.keymap.set({'n', 'i', 'v'}, '<C-_>', '<Cmd>set operatorfunc=CommentOperator<CR>g@')
-vim.keymap.set('n', '<leader>dvo', '<Cmd>DiffviewOpen<CR>')
-vim.keymap.set('n', '<leader>dvc', '<Cmd>DiffviewClose<CR>')
+vim.keymap.set('n', '<leader>gdo', '<Cmd>DiffviewOpen<CR>')
+vim.keymap.set('n', '<leader>gdc', '<Cmd>DiffviewClose<CR>')
 vim.keymap.set('n', '<leader>cd', ':cd ')
+vim.keymap.set('x', '<leader>p', '\"_dP')
+vim.keymap.set({'n','v'}, '<leader>y', '\"+y')
+vim.keymap.set({'n','v'}, '<leader>Y', '\"+Y')
+vim.keymap.set({'n','v'}, '<leader>d', '\"_d')
+vim.keymap.set({'n','v'}, '<leader>F', ':%s/\\<<C-r><C-w>\\>//g<left><left>')
+
+
 vim.keymap.set('n', '<leader>dt', function ()
     local status = vim.diagnostic.config().virtual_text
     vim.diagnostic.config({virtual_text = not status})

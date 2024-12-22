5c22cf0742fef1c152975a95a78a9e6927e646da
br0steen <osteensco@gmail.com>
2024-12-21 21:23:25 -0600

    fixed telescope grep

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -16,7 +16,7 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>tf', builtin.git_files,
     { desc = '[t]elescope project [f]iles, looks at files within git repo' })
 vim.keymap.set('n', '<leader>ts', function()
-        builtin.grep_string({ search = "" })
+        builtin.grep_string({ search = vim.fn.input("Grep > ") })
     end,
     { desc = '[t]elescopt project [s]earch, takes a string a searches for it in files within git repo' })
 vim.keymap.set('n', '<leader>td', builtin.diagnostics, { desc = '[t]elescope [d]iagnostics' })

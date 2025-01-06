f4f579f7d6aeb6de837eaf4c744aacc35529dd53
Scott Osteen <osteensco@gmail.com>
2025-01-06 14:31:32 -0600

    discord status'

--- after/plugin/discordStatus.lua
+++ after/plugin/discordStatus.lua
@@ -2,10 +2,8 @@ require('discordStatus').setup({
     openStatus = {
         "Programming",
         "Coding",
-        "Making something",
         "Neovim shenanigans",
         "Building stuff",
         "Developing software",
-        "Copy and pasting from chatGPT",
     }
 })
